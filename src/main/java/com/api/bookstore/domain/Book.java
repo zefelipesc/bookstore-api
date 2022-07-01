639ccbd9a95542fc3bd3a40cf429c5e6acf42b37
jose <jose.carvalho@dremio.com>
2022-07-01 17:34:07 -0300

    Creating domain classes

---
+++ src/main/java/com/api/bookstore/domain/Book.java
@@ -0,0 +1,76 @@
+package com.api.bookstore.domain;
+
+import java.util.Objects;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author_name;
+    private String text;
+
+    private Category category;
+
+    public Book() {
+    }
+
+    public Book(int id, String title, String author_name, String text, Category category) {
+        this.id = id;
+        this.title = title;
+        this.author_name = author_name;
+        this.text = text;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return getId() == book.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/com/api/bookstore/domain/Category.java
@@ -0,0 +1,63 @@
+package com.api.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Category {
+    private int id;
+    private String name;
+    private String description;
+
+    private final List<Book> books = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
