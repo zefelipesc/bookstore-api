32a5f369c4acec449318af8a401bc66ed8a841b9
jose felipe <josefelipesc86@gmail.com>
2022-07-08 09:50:35 -0300

    delete method to category

--- src/main/java/com/api/bookstore/Controllers/CategoryController.java
+++ src/main/java/com/api/bookstore/Controllers/CategoryController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +53,11 @@ public class CategoryController {
     public ResponseEntity<CategoryDTO> update(@PathVariable Integer id, @RequestBody CategoryDTO objDto){
         Category newObjt = categoryService.update(id, objDto);
         return ResponseEntity.ok().body(new CategoryDTO(newObjt));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
