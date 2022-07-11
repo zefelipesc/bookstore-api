b76c6650106358b13fac27d0ba7fe484e5de275e
jose felipe <josefelipesc86@gmail.com>
2022-07-11 11:10:17 -0300

    Fixing CORS policy

--- src/main/java/com/api/bookstore/Controllers/CategoryController.java
+++ src/main/java/com/api/bookstore/Controllers/CategoryController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("/{id}")
     public ResponseEntity<Category> findById(@PathVariable Integer id) {
         Category obj = categoryService.findById(id);
